31df41fa0b5a007e4d10c5d9e71d1571402200d9
saanspeur <evelin.ortega018@gmail.com>
2023-08-31 13:48:31 -0700

    added pages folder and corresponding files

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,14 @@
+import Header from "../Header";
+
+function Resume() {
+  return (
+    <div>
+      <Header title="Resume" bgcolor="pink" />
+      <a href="/Resume.pdf" target="_blank">
+        Click here to download!
+      </a>
+    </div>
+  );
+}
+
+export default Resume;
